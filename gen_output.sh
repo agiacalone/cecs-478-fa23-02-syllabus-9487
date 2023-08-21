f103aea2afdeddca86477218277f7658e1972895
Anthony Giacalone <anthony.giacalone@csulb.edu>
2023-08-21 13:37:32 -0700

    updated pdf

--- gen_output.sh
+++ gen_output.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pandoc README.md -o cecs-478-fa23-02-syllabus-9487.html --from markdown
-pandoc README.md -o cecs-478-fa23-02-syllabus-9487.pdf --from markdown --template eisvogel --listings
+pandoc README.md --template eisvogel -V linkcolor=blue -V header-includes:'\usepackage[export]{adjustbox} \let\includegraphicsbak\includegraphics \renewcommand*{\includegraphics}[2][]{\includegraphicsbak[frame,#1]{#2}}' -o cecs-478-fa23-02-syllabus-9487.pdf
 
