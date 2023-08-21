d37542bcf9fd04277d9a229cf9c36331c29853d0
Anthony Giacalone <anthony.giacalone@csulb.edu>
2023-08-21 13:25:29 -0700

    fixed pdf version

--- gen_output.sh
+++ gen_output.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pandoc README.md -o cecs-450-fa23-02-syllabus-9855.html --from markdown
-pandoc README.md -o cecs-450-fa23-02-syllabus-9855.pdf --from markdown --template eisvogel --listings
+pandoc README.md -o cecs-478-fa23-02-syllabus-9487.html --from markdown
+pandoc README.md -o cecs-478-fa23-02-syllabus-9487.pdf --from markdown --template eisvogel --listings
 
